fe943b7a41a5b51a8bcacaec71df28b53284a44c
vishal <outliervishal@gmail.com>
2026-07-26 02:02:07 +0530

    fix deployment update

--- backend/Routes/Resume.js
+++ backend/Routes/Resume.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ResumeController = require("../Controllers/resume");
+const ResumeController = require("../Controllers/Resume");
 
 const { upload } = require("../utils/multer");
 router.post("/addResume", upload.single("resume"), ResumeController.addResume);
