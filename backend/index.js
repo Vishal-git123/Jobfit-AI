adf1cc6d90e41f1a006c6813734a64c596ae22ff
vishal <outliervishal@gmail.com>
2026-07-26 01:43:52 +0530

    fix changes

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,29 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
-const PORT = 4000;
+
+const PORT = process.env.PORT || 4000;
+
 require("./conn");
+
 app.use(express.json());
+
 app.use(
   cors({
     credentials: true,
     origin: "http://localhost:5173",
   }),
 );
+
 const UserRoutes = require("./Routes/user");
-const ResumeRoutes = require("./Routes/resume");
+const ResumeRoutes = require("./Routes/Resume");
+
 app.use("/api/user", UserRoutes);
 app.use("/api/resume", ResumeRoutes);
-app.listen(PORT, () => {
-  console.log("backend is running on port", PORT);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Backend is running on port", PORT);
 });
